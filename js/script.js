fb26eb3b4075a07e7a2a5977f6086d9d17dc490a
Matias Salvador Chidiak <matiasschidiak@gmail.com>
2025-11-02 16:41:29 -0300

    Agregando contenedor y animación de aparición en servicios.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,29 @@
+// aparicion al scrollear en el cartel "sobre-nosotros"
 const sobreNosotros = document.querySelector('.sobre-nosotros');
-
-const observer = new IntersectionObserver((entradas) => {
-    entradas.forEach((entrada) => {
-        if (entrada.isIntersecting) {
-            sobreNosotros.classList.add('visible');
-            observer.unobserve(sobreNosotros);
+if (sobreNosotros) {
+    const observer1 = new IntersectionObserver((entradas) => {
+        entradas.forEach((entrada) => {
+            if (entrada.isIntersecting) {
+                sobreNosotros.classList.add('visible');
+                observer1.unobserve(sobreNosotros);
             }
         });
-}, { threshold: 0.2 });
+    }, { threshold: 0.2 });
 
-observer.observe(sobreNosotros);
+    observer1.observe(sobreNosotros);
+}
+
+// aparicion al scrollear en servicios
+const contenedor = document.querySelector('.contenedor');
+if (contenedor) {
+    const observer2 = new IntersectionObserver((entradas) => {
+        entradas.forEach((entrada) => {
+            if (entrada.isIntersecting) {
+                contenedor.classList.add('visible');
+                observer2.unobserve(contenedor);
+            }
+        });
+    }, { threshold: 0.2 });
+
+    observer2.observe(contenedor);
+}
